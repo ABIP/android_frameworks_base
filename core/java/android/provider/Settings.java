0903674b48172d8fc21b1c4df687fbfd72d1b81d
wszfer <wszfer@gmail.com>
2013-12-21 04:16:33 +0800

    FB: Add Screen Recording (4/4) See:http://forum.xda-developers.com/showthread.php?t=2547196
    Change-Id: I9d8c5d3d2efd4e95d3fda113deec1f3a8312b393

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5953,6 +5953,24 @@ public final class Settings {
         public static final String PERFORMANCE_PROFILE = "performance_profile";
 
         /**
+         * Width and height of output vide expressed in WxH
+         * @hide
+         */
+        public static final String SCREEN_RECORDER_OUTPUT_DIMENSIONS = "screen_recorder_output_dimensions";
+
+        /**
+         * Screen recorder framerate in bits per second
+         * @hide
+         */
+        public static final String SCREEN_RECORDER_BITRATE = "screen_recorder_bitrate";
+
+        /**
+         * Whether to include audio when recording a video
+         * @hide
+         */
+        public static final String SCREEN_RECORDER_RECORD_AUDIO = "screen_recorder_record_audio";
+
+        /**
          * App-based performance profile selection
          * @hide
          */
